703d0a07d81a56bdda0e4e95231bfdb683169d9c
Liz2222 <adhiamboliz3@gmail.com>
2022-05-17 23:49:08 +0300

    Added quotes to home header

--- blogip/main/routes.py
+++ blogip/main/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, Blueprint
 from blogip.models import Post
+from blogip.request import get_quotes
+
 
 main = Blueprint('main', __name__)
 
@@ -9,7 +11,8 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    randomquote = get_quotes()
+    return render_template('home.html', posts=posts,randomquote=randomquote)
 
 
 @main.route("/about")
